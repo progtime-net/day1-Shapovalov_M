cf684c7d6105d0440d4744a31e1688e3385f1c8f
MARKPRO44 <119742977+MARKPRO4444@users.noreply.github.com>
2023-08-11 18:00:14 +0300

    init

--- Lesson_2/Program.cs
+++ Lesson_2/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, what's your name?");
+
+        string name = Console.ReadLine();
+
+        Console.WriteLine(name);
+    }
+}
